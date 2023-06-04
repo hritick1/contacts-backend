1892002c250128ac3cf0d1e2e00ec13b913b748c
hritick1 <hritick.12016375@lpu.in>
2023-06-04 15:09:25 +0530

    added deployed origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,4 +1,4 @@
-const allowedOrigins=['http://localhost:3000','http://localhost:3001','https://course-app-hritick1.vercel.app','https://course-app-jet.vercel.app'];
+const allowedOrigins=['http://localhost:3000','http://localhost:3001','https://contacts-vault.vercel.app/','https://contacts-vault-hritick1.vercel.app/','https://contacts-vault-git-main-hritick1.vercel.app/'];
 
 
 module.exports=allowedOrigins;
